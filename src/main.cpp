ee468a8e0143d499dc52bfac10e8585d139b7dc9
Daniel Carns <carns.daniel1@gmail.com>
2023-11-02 11:34:46 -0600

    Added overridden ble device callback, added basic ble scanning to startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+ 
+class DeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+	void onResult(BLEAdvertisedDevice advertisedDevice) {
+		ESP_LOGD(LOG_TAG, "Advertised Device: %s", advertisedDevice.toString().c_str());
+	}
+};
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println("Scanning...");
+
+  BLEDevice::init("theDashBoard");
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new DeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+  BLEScanResults foundDevices = pBLEScan->start(30);
+  Serial.println("Scan done!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop() {}
